3c995f8401e2a15f29fc32040ab2d4a241ff1224
Choosak Jittpornwong <choosak.j@magicsoftware.co.th>
2023-08-16 10:03:15 +0700

    Add project files.

---
+++ TestHtmlToImage/Program.cs
@@ -0,0 +1,23 @@
+﻿using HtmlToImage;
+using System.IO;
+using System.Text;
+
+namespace TestHtmlToImage
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // From HTML string
+            var converter = new HtmlConverter();
+            var html = "<div><strong>Hello</strong> World!</div>";
+            var htmlBytes = converter.FromHtmlString(html, 500, ImageFormat.Png, 100);
+            File.WriteAllBytes("D:\\hello.png", htmlBytes);
+
+            // From URL
+            //var converter = new HtmlConverter();
+            //var urlBytes = converter.FromUrl("http://localhost/", 800, format: ImageFormat.Png, quality: 100);
+            //File.WriteAllBytes("D:\\localhost.png", urlBytes);
+        }
+    }
+}
